6ac604caa9d32883b4345db6a25388a4ef112a5a
KISO <h.kanai.5@gmail.com>
2016-04-24 03:56:19 +0000

    Day6 Homework2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all(userlist)
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+private
+    def userlist
+        params.require(:users).permit(:name, :profile)
+    end
+
 end
