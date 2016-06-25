1ca8da84740f584fc63c32199401281b28ec6ba2
KISO <h.kanai.5@gmail.com>
2016-06-25 15:20:58 +0000

    Day10 Homework7

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ ActionMailer::Base.smtp_settings =
 :password => "sujkydsn2730",
 :domain => "heroku.com", 
 :address => "smtp.sendgrid.net", 
-:port => 2525, 
+:port => 587, 
 :authentication => :plain, 
 :enable_starttls_auto => true 
 }
