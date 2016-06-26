20cccb75b9a2fd37984419a6528faaaa29df4ecc
KISO <h.kanai.5@gmail.com>
2016-06-26 07:37:25 +0000

    Day10 Homework9

--- config/environment.rb
+++ config/environment.rb
@@ -7,11 +7,11 @@ Rails.application.initialize!
 ActionMailer::Base.delivery_method = :smtp 
 ActionMailer::Base.smtp_settings =
 { 
-:user_name => "app48934552@heroku.com", 
-:password => "sujkydsn2730",
-:domain => "heroku.com", 
-:address => "smtp.sendgrid.net", 
-:port => 2525, 
+:user_name => ENV['SENDGRID_USERNAME'], 
+:password => ENV['SENDGRID_PASSWORD'],
+:domain => 'heroku.com', 
+:address => 'smtp.sendgrid.net', 
+:port => 587, 
 :authentication => :plain, 
 :enable_starttls_auto => true 
 }
